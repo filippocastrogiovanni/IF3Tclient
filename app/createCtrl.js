304e4b69dba22ac9eece2f0e4cfb57bdfc2172a5
Alessio <alex92_v@hotmail.it>
2016-06-02 14:08:16 +0200

    NEW RECIPE IS RESPONSIVE AND ALMOST FINISHED2

---
+++ app/createCtrl.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Alessio on 22/05/2016.
+ */
+
+
+if3tApp.controller('CreateRecipeController', ['$scope', '$rootScope', '$routeParams', '$location',
+    function ($scope, $rootscope, $routeParams, $location) {
+    }
+]);
