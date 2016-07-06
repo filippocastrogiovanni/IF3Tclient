de6b172e1bbdb5e8ede62e8d8943246d0fee088f
filippocastrogiovanni <filippo.castrogiovanni@gmail.com>
2016-07-06 14:56:49 +0200

    login - signup

--- app/homeCtrl.js
+++ app/homeCtrl.js
@@ -5,7 +5,8 @@
 if3tApp.controller('HomeController', ['$scope', '$rootScope', '$routeParams', '$location',
     function ($scope, $rootscope, $routeParams, $location) {
         $rootscope.curpage = "home";
-        var offset = new Date().getTimezoneOffset();
-        console.log(offset);
+        $rootscope.timezone = new Date().getTimezoneOffset();
+        $rootscope.timezone = $rootscope.timezone /60;
+        console.log($rootscope.timezone);
     }
 ]);
