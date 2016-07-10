2bbe961df65d15105dad9f4e4683dc8a5b894310
Planet910 <planet910@gmail.com>
2016-07-10 10:10:23 +0200

    channels

--- app/myrecipesCtrl.js
+++ app/myrecipesCtrl.js
@@ -2,15 +2,22 @@
  * Created by Filippo on 24/05/2016.
  */
 
-if3tApp.controller('MyRecipesController', ['$scope', '$rootScope', '$routeParams', '$location', '$http',
-    function ($scope, $rootScope, $routeParams, $location, $http) {
+if3tApp.controller('MyRecipesController', ['userFactory','$scope', '$rootScope', '$routeParams', '$window', '$http',
+    function (userFactory, $scope, $rootScope, $routeParams, $window, $http) {
         $rootScope.curpage = "myrecipes";
 
-        $http.get($rootScope.ipServer + "/recipes/1")
+        if(!userFactory.isAuthenticated())
+            $window.location.href = "#/home";
+
+        $http({
+            method: 'GET',
+            url: $rootScope.ipServer + '/recipes/' + userFactory.getProfile().id,
+            headers: {'Content-Type': 'application/json', 'authorization': userFactory.getAuthorization()}
+        })
             .then(
                 function success(response){
                     $scope.recipes = response.data;
-                    console.log($scope.recipes);
+                    //console.log($scope.recipes);
                 },
                 function error(error){
                     console.log(error.statusText);
@@ -22,20 +29,42 @@ if3tApp.controller('MyRecipesController', ['$scope', '$rootScope', '$routeParams
         };
 
         $scope.toggleRecipePublic = function(recipe){
-            recipe.isPublic = !recipe.isPublic;
-            $http.put($rootScope.ipServer + "/update_recipe", recipe)
+            $http({
+                method: 'PUT',
+                dataType: 'json',
+                url: $rootScope.ipServer + '/publish_recipe',
+                headers: {'Content-Type': 'application/json', 'authorization': userFactory.getAuthorization()},
+                data: angular.toJson(recipe)
+            })
                 .then(
-                    function success(response){
+                    function successCallback(response) {
                         console.log(response);
+                        recipe.isPublic = !recipe.isPublic;
                     },
-                    function error(error){
+                    function errorCallback(error) {
                         console.log(error);
-                    }
-                );
+                    });
+
         };
 
-        $scope.toggleRecipeEnabled = function(recipeId){
-
+        $scope.toggleRecipeEnabled = function(recipe){
+            $http({
+                method: 'PUT',
+                dataType: 'json',
+                url: $rootScope.ipServer + '/enable_recipe',
+                headers: {'Content-Type': 'application/json', 'authorization': userFactory.getAuthorization()},
+                data: angular.toJson(recipe)
+            })
+                .then(
+                    function success(response){
+                        //console.log(response);
+                        recipe.isEnabled = !recipe.isEnabled;
+                    },
+                    function error(error){
+                        //$window.alert(error.data.message);
+                        console.log("ciao");
+                    }
+                );
         };
     }
 ]);
