58053c33e89319a5570cf1a564fc3158f0a6e021
Sebastiano <sebastiano.gazze@gmail.com>
2016-08-08 00:33:54 +0200

    Twitter channel 01

--- app/channelsCtrl.js
+++ app/channelsCtrl.js
@@ -2,53 +2,67 @@
  Created by Filippo on 24/05/2016.
  */
 
-if3tApp.controller('ChannelsController', ['$scope', '$rootScope', '$routeParams', '$window', '$http', 'userFactory',
-    function ($scope, $rootScope, $routeParams, $window, $http, userFactory) {
+if3tApp.controller('ChannelsController', ['$scope', '$rootScope', '$routeParams', '$window', '$http', 'userFactory', 'messageFactory',
+    function ($scope, $rootScope, $routeParams, $window, $http, userFactory, messageFactory)
+    {
         $rootScope.curpage = "channels";
+        messageFactory.showLoading();
 
-        $http.get($rootScope.ipServer + "/channels")
-            .then(
-                function success(response){
-                    $scope.channels = response.data;
-                },
-                function error(error){
-                    console.log(error);
-                }
-            );
+        $http.get($rootScope.ipServer + "/channels").then
+        (
+            function success(resp)
+            {
+                messageFactory.hideLoading();
+                $scope.channels = resp.data;
+            },
+            function error(resp)
+            {
+                messageFactory.hideLoading();
+                messageFactory.showError(resp.data.code + " - " + resp.data.reasonPhrase, resp.data.message);
+            }
+        );
 
         $scope.channelURL = "";
         $scope.showDetailChannel = false;
-        $scope.selectChannel = function(channel){
+
+        $scope.selectChannel = function(channel)
+        {
             $http({
                 method: 'GET',
                 dataType: 'json',
                 url: $rootScope.ipServer + '/' + channel.keyword + '/auth',
-                headers: {'Content-Type': 'application/json', 'authorization': userFactory.getAuthorization()}
-            })
-                .then(function successCallback(response) {
-                        $scope.channelURL = response.data.message;
-                    },
-                    function errorCallback(error) {
-                        console.log(error);
-                        console.log("Errore nel richiedere il channel URL");
-                    });
-            if($scope.selectedChannel == null || $scope.selectedChannel == channel){
+                headers: { 'Content-Type': 'application/json', 'authorization': userFactory.getAuthorization() }
+            }).then
+            (
+                function successCallback(resp)
+                {
+                    $scope.channelURL = resp.data.message;
+                },
+                function errorCallback(resp)
+                {
+                    messageFactory.showError(resp.data.code + " - " + resp.data.reasonPhrase, resp.data.message);
+                }
+            );
+
+            if ($scope.selectedChannel == null || $scope.selectedChannel == channel)
+            {
                 $scope.selectedChannel = channel;
                 $scope.showDetailChannel = !$scope.showDetailChannel;
                 return;
             }
+
             $scope.showDetailChannel = false;
-           $scope.selectedChannel = channel;
+            $scope.selectedChannel = channel;
             $scope.showDetailChannel = true;
 
         };
 
-        $scope.backToChannels = function(){
+        $scope.backToChannels = function() {
             $scope.showDetailChannel = false;
         };
 
-        $scope.loadPage = function(){
-            console.log($scope.channelURL);
+        $scope.loadPage = function()
+        {
             $window.open($scope.channelURL,"_blank","location=no," +
                 "menubar=no," +
                 "toolbar=no," +
@@ -59,6 +73,5 @@ if3tApp.controller('ChannelsController', ['$scope', '$rootScope', '$routeParams'
                 "top=100,left=300," +
                 "width=550,height=550");
         };
-
     }
 ]);
